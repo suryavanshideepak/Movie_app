31d718e48e888416f6a32db0e028e599c6d8b95c
Deepika Maheshwari <deepika.maheshwari@decipherzone.com>
2025-01-27 14:36:43 +0530

    movie

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,66 +1,89 @@
-import React, { useEffect, useState } from 'react'
-import { NavLink, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
 
 const MovieDetails = () => {
-    const [movieData, setMovieData] = useState({})
-    const [loading, setLoading] = useState(false)
-    let { id } = useParams()
-    const getData = async () => {
-        setLoading(true)
-        let  urlOfApi = `https://www.omdbapi.com/?i=${id}&apikey=a871e09f`
-        const url = await fetch(urlOfApi);
-        const data = await url.json();
-        setMovieData(data)
-        setLoading(false)
-    }
+  const [movieData, setMovieData] = useState({});
+  const [loading, setLoading] = useState(false);
+  let { id } = useParams();
 
-    useEffect(() => {
-        if(id){
-           getData()
-        }
-    },id)
-    if (loading) {
-        return (
-          <>
-            <div className="d-flex justify-content-center">
-              <div className="spinner-border" role="status">
-                <span className="sr-only">Loading...</span>
+  const getData = async () => {
+    setLoading(true);
+    let urlOfApi = `https://www.omdbapi.com/?i=${id}&apikey=a871e09f`;
+    const url = await fetch(urlOfApi);
+    const data = await url.json();
+    setMovieData(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (id) {
+      getData();
+    }
+  }, [id]);
+
+  if (loading) {
+    return (
+      <>
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="container py-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+          <div className="card shadow-lg">
+            <div className="row g-0">
+              {/* Poster Section */}
+              <div className="col-md-4 text-center">
+                <img
+                  className="card-img-top img-fluid p-3"
+                  src={movieData.Poster}
+                  alt={movieData.Title}
+                />
+              </div>
+
+              {/* Movie Details Section */}
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title text-primary">{movieData.Title}</h5>
+                  <p className="card-text">
+                    <strong>Year:</strong> {movieData.Year}
+                  </p>
+                  <p className="card-text">
+                    <strong>Type:</strong> {movieData.Type}
+                  </p>
+                  <p className="card-text">
+                    <strong>Actors:</strong> {movieData.Actors}
+                  </p>
+                  <p className="card-text">
+                    <strong>BoxOffice:</strong> {movieData.BoxOffice}
+                  </p>
+                  <p className="card-text">
+                    <strong>Country:</strong> {movieData.Country}
+                  </p>
+                  <p className="card-text">
+                    <strong>Released:</strong> {movieData.Released}
+                  </p>
+                  <p className="card-text">
+                    <strong>Plot:</strong> {movieData.Plot}
+                  </p>
+                  <NavLink to="/home" className="btn btn-danger btn-sm mt-3">
+                    Go Back
+                  </NavLink>
+                </div>
               </div>
             </div>
-          </>
-        );
-      }
-  return (
-    <div>
-        <div className="container d-flex justify-content-center align-items-center py-5">
-        <div className="card ">
-          <div className="d-flex">
-            <div>
-              <img className="card-img-top" src={movieData.Poster} alt="" width={100} />
-            </div>
-           <div className="card-body">
-            <div className='row'>
-                <div className='col-md-6'>
-                    <h5 className="card-title">{movieData.Title}</h5>
-                    <p className="card-text">Year: {movieData.Year}</p>
-                    <p>Type: {movieData.Type}</p>
-                    <p>Actors : {movieData.Actors}</p>
-                </div>
-                <div className='col-md-6'>
-                    <p>BoxOffice : {movieData.BoxOffice} </p>
-                    <p>Country : {movieData.BoxOffice} </p>
-                    <p>Released : {movieData.Released}</p>
-                </div>
-            </div>
-                
-                <p>Plot : {movieData.Plot}</p>
-                <NavLink to={"/home"} className="btn btn-danger btn-sm" >Go Back</NavLink>
-            </div>
           </div>
-        </div> 
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieDetails
+export default MovieDetails;
